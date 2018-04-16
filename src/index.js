8beda18803915fb86324de3371ba002f172058b8
Maxwell Krause <maxwell.l.krause@gmail.com>
2018-04-16 14:15:49 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import program from 'commander';
+import figlet from 'figlet';
+
+const header = figlet.textSync('NodeCli!', { font: 'ogre', });
+
+program
+  .version('0.1.0')
+  .description(`${header}
+    Place holder text for your cli app`)
+  .command('test')
+  .option('-o, --option', 'sample option')
+
+  .action((cmd) => {
+    console.log('COMMAND!', cmd);
+  });
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
